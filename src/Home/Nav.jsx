e7d7f8fc91c9e9b06160661d5df1af5d11176926
Shahadat1530 <155170141+Shahadat1530@users.noreply.github.com>
2024-12-23 13:12:51 +0600

    added conditional in navbar, also added logout btn

--- src/Home/Nav.jsx
+++ src/Home/Nav.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import React, { useContext } from 'react';
+import { NavLink } from 'react-router-dom';
+import { AuthContext } from '../provider/AuthProvider';
 
-const Nav = () => {
+const NavBar = () => {
+    const { user, logOut } = useContext(AuthContext);
+
     const links = <>
-        <li><Link to='/'>Home</Link></li>
-    </>
+        <li><NavLink to='/' className="nav-link px-3 py-2 rounded-md text-gray-800 hover:text-white transition duration-300 ease-in-out">Home</NavLink></li>
+    </>;
+
     return (
         <div>
             <div className="navbar bg-base-100">
@@ -26,11 +30,11 @@ const Nav = () => {
                         </div>
                         <ul
                             tabIndex={0}
-                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-50 mt-3 w-52 p-2 shadow">
                             {links}
                         </ul>
                     </div>
-                    <p className="text-xl">Libro Vault</p>
+                    <a className="btn btn-ghost text-xl">Libro Vault</a>
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal px-1">
@@ -38,14 +42,35 @@ const Nav = () => {
                     </ul>
                 </div>
                 <div className="navbar-end">
-                    <>
-                        <NavLink to='/login' className="bg-gradient-to-r from-pink-500 to-yellow-500 text-white font-semibold py-2 px-4 rounded-md mr-2 transition duration-300 ease-in-out hover:bg-gradient-to-r hover:from-yellow-500 hover:to-pink-500">Login</NavLink>
-                        <NavLink to='/register' className="bg-gradient-to-r from-pink-500 to-yellow-500 text-white font-semibold py-2 px-4 rounded-md transition duration-300 ease-in-out hover:bg-gradient-to-r hover:from-yellow-500 hover:to-pink-500">Register</NavLink>
-                    </>
+                    {user && user?.email ? (
+                        <div className="flex gap-2 items-center">
+                            <div className="relative group">
+                                <img
+                                    className="w-10 rounded-full"
+                                    src={user?.photoURL}
+                                    alt="User Avatar"
+                                />
+                                <p className="absolute bottom-0 left-0 right-0 text-center text-white bg-black bg-opacity-60 opacity-0 group-hover:opacity-100 transition duration-300">
+                                    {user.displayName}
+                                </p>
+                            </div>
+                            <button
+                                onClick={logOut}
+                                className="bg-gradient-to-r from-pink-500 to-yellow-500 text-white font-semibold py-2 px-4 rounded-md transition duration-300 ease-in-out hover:bg-gradient-to-r hover:from-yellow-500 hover:to-pink-500"
+                            >
+                                Log-Out
+                            </button>
+                        </div>
+                    ) : (
+                        <>
+                            <NavLink to='/login' className="bg-gradient-to-r from-pink-500 to-yellow-500 text-white font-semibold py-2 px-4 rounded-md mr-2 transition duration-300 ease-in-out hover:bg-gradient-to-r hover:from-yellow-500 hover:to-pink-500">Login</NavLink>
+                            <NavLink to='/register' className="bg-gradient-to-r from-pink-500 to-yellow-500 text-white font-semibold py-2 px-4 rounded-md transition duration-300 ease-in-out hover:bg-gradient-to-r hover:from-yellow-500 hover:to-pink-500">Register</NavLink>
+                        </>
+                    )}
                 </div>
             </div>
         </div>
     );
 };
 
-export default Nav;
+export default NavBar;
